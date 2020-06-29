aa1ac1d304d9c632294e40aa61da66726e0666db
Iury <iurymig.sht@gmail.com>
2020-06-29 19:54:48 -0300

    Sumindo botao de ganhar vida qdo tem 5 vidas

--- Assets/Scripts/ControladorJogo.cs
+++ Assets/Scripts/ControladorJogo.cs
@@ -119,6 +119,7 @@ public class ControladorJogo : MonoBehaviour
     public static void UpdateLife(int value)
     {
         lifes += value;
+        MenuPauseComp.handleBotaoGanharVida.OnNext(lifes);
         LifeImageComp.UpdateImage();
         if(lifes == 0)
         {
